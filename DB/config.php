4bd4592c90d2599cba304b729efc647c490e9fca
Nattapat23 <Nattapat23r@gmail.com>
2025-10-17 15:44:13 +0700

    Update config.php

--- DB/config.php
+++ DB/config.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "127.0.0.1";   
 $username = "root";         
-$password = "nattapat23";              
+$password = "";   // your pass           
 $dbname = "Luminix";         
 
 
